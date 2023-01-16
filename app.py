13c583f99f0589a96940da7fd1e12f822e9b6117
micheal1woo <53082557+micheal1woo@users.noreply.github.com>
2023-01-16 14:09:10 +0900

    ãkey-upload

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
 from flask import Flask, render_template, request, jsonify
-app = Flask(__name__, template_folder="templates")
-
+from key import *
 from pymongo import MongoClient
 import certifi
 
+
 ca = certifi.where()
+app = Flask(__name__, template_folder="templates")
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.gvsa3p3.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
+
+@app.route("/review", methods=["POST"])
+def s3_upload_img():
+    review_route = request.form['image_give']
+    title = request.form['title_give']
+    data = open(review_route + title, 'rb')
+    # data = open(review_route + f + '.jpg', 'rb')
+    s3 = boto3.resource(
+        's3',
+        aws_access_key_id="AKIAT7KMYIXV4MQ5KCPJ",
+        aws_secret_access_key="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",
+        config=Config(signature_version='s3v4')
+    )
+    s3.Bucket(BUCKET_NAME).put_object(
+        Key=title, Body=data, ContentType='image/jpg')
+
+    return jsonify({'msg': '등록완료'})
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/reviewWritePage.html')
 def reviewwritepage():
@@ -21,6 +42,7 @@ def reviewwritepage():
 @app.route('/reviewPage.html')
 def reviewpage():
     return render_template('reviewPage.html')
+
 
 @app.route('/etcReview.html')
 def etcReview():
@@ -41,9 +63,11 @@ def smartPhoneReview():
 def smartWatchReview():
     return render_template('smartWatchReview.html')
 
+
 @app.route('/TabletReview.html')
 def TabletReview():
     return render_template('TabletReview.html')
+
 
 @app.route("/review", methods=["POST"])
 def web_review_post():
